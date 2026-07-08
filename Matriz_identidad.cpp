8b3bd62f4d2d023fd60d9ecc4b9a815a69a636d2
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-08 09:59:36 -0500

    Matriz_identidad: segundo intento

--- Matriz_identidad.cpp
+++ Matriz_identidad.cpp
@@ -2,23 +2,25 @@
 using namespace std;
 
 int main (){
-
-	cout << "Ingrese el valor de n para una matriz nxn">>;
+	
+	int n;
+	
+	cout << "Ingrese el valor de n para una matriz nxn"<<;
 	cin >> n;
 	
 	int matriz [100][100];
 	
 	for (int fila = 0; fila < n; fila ++){
-		for (int columna = 0; columa < n; columna++){
+		for (int columna = 0; columna < n; columna++){
 			if (fila == columna){
-				matris [fila][columna] = 1 //es la diagonal principal
+				matriz [fila][columna] = 1; //es la diagonal principal
 			} else {
 				matriz [fila][columna] = 0; //lo demás
 			}
 		}
 	}
 	cout << "Matriz de identidad de" << n << ": ";
-	for (int columna = 0; fila < n; fila++){
+	for (int fila = 0; fila < n; fila++){
 		for (int columna = 0; columna < n; columna++){
 			cout << matriz[fila][columna] << " ";
 		}
