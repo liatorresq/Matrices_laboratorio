865a7b7c41d823dda51120838d48aa98a835ea7c
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-08 10:21:48 -0500

    matriz_transpuesta: primer intento (no finalizado)

---
+++ matriz_transpuesta.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int m, n;
+	cout << "Ingrese el numero de filas (m): ";
+	cin >> m;
+	cout << "Ingrese el numero de columnas (n): ";
+	cin >> n;
+	
+}
